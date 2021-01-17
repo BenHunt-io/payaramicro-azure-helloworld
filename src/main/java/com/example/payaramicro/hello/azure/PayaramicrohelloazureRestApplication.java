7d274085a8fb07ca8da82863b04cd670901045eb
Ben Hunt <whunt@openlending.com>
2021-01-16 23:35:44 -0600

    init repo

---
+++ src/main/java/com/example/payaramicro/hello/azure/PayaramicrohelloazureRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.payaramicro.hello.azure;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class PayaramicrohelloazureRestApplication extends Application {
+}
